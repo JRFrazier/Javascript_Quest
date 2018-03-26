8825466f112edb5c06871d05f60b58b9597737da
JRFrazier <jrfrazier21@gmail.com>
2018-03-26 07:45:46 -0400

    nothing

---
+++ Array.map.js
@@ -0,0 +1,4 @@
+//Map 
+
+//Produces a new array of values by mapping each value in list trhough a transformation function (iterator).
+//Each invocation of iterator is called with three arbuments: (element, indes, list). if "list" is a JavaScript object, itterator's aruments will be (value,key,list)
